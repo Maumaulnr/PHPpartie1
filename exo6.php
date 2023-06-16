51f3c417257e5041645f4117514e598ae565eeb2
Maurane Lenoir <maurane1831@gmail.com>
2023-06-16 15:56:58 +0200

    15h56

--- exo6.php
+++ exo6.php
@@ -10,23 +10,28 @@ Le montant de la facture à régler est de : 59.94 €
 </p>
 
 <!-- montant TVA pour un article = 2€  (ALT+0128 = €) 
-Montant TTC = 11,99 €
+Montant TTC = 11,99 € (9,99+2)
 (prix TTC / 120) x 20 = 59,94 / 120 = 0,4995 * 20 = 9,99€ (montant TVA)
 prix HT = 9,99€ (59,94 - 9,99)
+Montant total = (9,99 + 2) x 5 = 11,99 x 5 = 59,95€
 -->
 
 
 <?php
 
 // Déclaration variable
-$nbArticle;
-$prixHT;
+$nbArticle;  // 5
+$prixHT;     // 9,99
 $tauxTVA;   // décimal (0.2)
+$montantTotal;
 
 
 
 // condition et affichage
 
+$montantTotal = $nbArticle * $prixHT + $nbArticle * $prixHT * $tauxTVA;
+
+echo "Le total TTC est de $totalTTC €<br>";
 
 
 
