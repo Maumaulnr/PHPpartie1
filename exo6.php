f910893a272e5a78e6963e00aaeb964b565061b6
Maurane Lenoir <maurane1831@gmail.com>
2023-06-16 15:02:26 +0200

    15h02

--- exo6.php
+++ exo6.php
@@ -1,18 +1,27 @@
 <h1>Exo 6 PHP</h1>
 
 <p>Ecrire un algorithme permettant de calculer un montant de facture à régler à partir de la quantité 
-d’articles, son prix hors taxe et un taux de TVA (exprimé en décimal. Ex : 20 % -> 0.2)
+d'articles, son prix hors taxe et un taux de TVA (exprimé en décimal. Ex : 20 % -> 0.2)
 Affichage :
-Prix unitaire de l’article : 9.99 €
- Quantité : 5 
+Prix unitaire de l'article : 9.99 € (HT)
+Quantité : 5 
 Taux de TVA : 0.2
 Le montant de la facture à régler est de : 59.94 €
 </p>
+
+<!-- montant TVA pour un article = 2€  (ALT+0128 = €) 
+Montant TTC = 11,99 €
+(prix TTC / 120) x 20 = 59,94 / 120 = 0,4995 * 20 = 9,99€ (montant TVA)
+prix HT = 9,99€ (59,94 - 9,99)
+-->
 
 
 <?php
 
 // Déclaration variable
+$nbArticle;
+$prixHT;
+$tauxTVA;   // décimal (0.2)
 
 
 
