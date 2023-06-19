6ee6dc347f0197d9853ded46ddb411a0254324fe
Maurane Lenoir <maurane1831@gmail.com>
2023-06-19 09:31:05 +0200

    9h30

--- exo8.php
+++ exo8.php
@@ -1,6 +1,6 @@
 <h1>Exo 8 PHP</h1>
 
-<!-- http://localhost/LENOIR_Maurane/ALGO PHP/partie1/exo8.php -->
+<!-- http://localhost/LENOIR-Maurane/ALGO-PHP/partie1/exo8.php -->
 
 <p>
 Ecrire un algorithme qui renvoie la table de multiplication d'un nombre passé en paramètre sous la forme: <br>
@@ -13,7 +13,7 @@ Remarque: proposer 2 solutions avec 2 types de boucles. <br>
 </p>
 
 <!-- 
-
+Pour i allant de 0 à 10
 -->
 
 <h2>Résultat</h2>
@@ -22,12 +22,15 @@ Remarque: proposer 2 solutions avec 2 types de boucles. <br>
 
 
 // Déclaration variable
-
-
+$n = 8;    // chiffre à multiplier  
+$i;     // nombre de boucle qui va jusqu'à 10
+$resultat;
 
 // condition et affichage
 
-
+for ($i=1; $i <= 10; $i++) { 
+    echo "$i". "*". "$n". " ". "=". " ". $i*$n. "<br>";
+}
 
 
 ?>
