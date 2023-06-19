dae36058999dffa7a8b175de300603f6b3e25498
Maurane Lenoir <maurane1831@gmail.com>
2023-06-19 23:10:05 +0200

    23H09 le 19/06/23

--- exo13.php
+++ exo13.php
@@ -34,6 +34,7 @@ $moyenneGeneral;
 
 // condition et affichage
 
+    // la fonction créer est : calculerMoyenne
 function calculerMoyenne($eleve) : float {
     $nbNotes = count($eleve);   // compte le nombre de notes = 9
     $sommeNotes = array_sum($eleve);    // additionne les notes = 101
@@ -41,8 +42,6 @@ function calculerMoyenne($eleve) : float {
 
     return $moyenneGeneral;
 }
-
-
 
 foreach($eleve as $prenom => $moyenneGeneral) {
     echo "Sa moyenne générale est donc de : ". calculerMoyenne([10, 12, 8, 19, 3, 16, 11, 13, 9])."<br>";
